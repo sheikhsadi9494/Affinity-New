8caaace6392820d7741c9fe40a7319e66c972e83
sadi3434 <95516977+sadi3434@users.noreply.github.com>
2022-06-17 19:43:19 +0600

    changes

--- assets/js/containerSpacefecation.js
+++ assets/js/containerSpacefecation.js
@@ -27,39 +27,130 @@ function setUpsTabs () {
   })
 
   
+
+  
 // cross text in table 
 
   document.getElementById('v-pills-home-tab').addEventListener('click', function() {
-    const firstText = 'Container Specification: Standard Container';
+    const firstText = 'Standard Container';
     const firstValue = firstText;
     const currentText = document.getElementById('cs-text');
     currentText.innerText = firstValue;
 })
 
   document.getElementById('v-pills-profile-tab').addEventListener('click', function() {
-    const secondText = 'Container Specification: Refrigerated Container';
+    const secondText = 'Refrigerated Container';
     const secondValue = secondText;
     const currentText = document.getElementById('cs-text');
     currentText.innerText = secondValue;
 })
 
   document.getElementById('v-pills-disabled-tab').addEventListener('click', function() {
-    const thirdText = 'Container Specification: Open Top Container';
+    const thirdText = 'Open Top Container';
     const thirdValue = thirdText;
     const currentText = document.getElementById('cs-text');
     currentText.innerText = thirdValue;
 })
 
   document.getElementById('v-pills-messages-tab').addEventListener('click', function() {
-    const FourthText = 'Container Specification: Flat Rack Container';
+    const FourthText = 'Flat Rack Container';
     const FourthValue = FourthText;
     const currentText = document.getElementById('cs-text');
     currentText.innerText = FourthValue;
   })
 
   document.getElementById('v-pills-settings-tab').addEventListener('click', function() {
-    const fifthText = 'Container Specification: Tank Container';
+    const fifthText = 'Tank Container';
     const fifthValue = fifthText;
     const currentText = document.getElementById('cs-text');
     currentText.innerText = fifthValue;
 })
+
+
+// Standard Container crosstext 
+document.getElementById('st-20ft').addEventListener('click', function() {
+  const containerText = '(Standard Container: 20 Ft)';
+  const containerValue = containerText;
+  const currentText = document.getElementById('sub-text');
+  currentText.innerText = containerValue
+})
+document.getElementById('st-40ft').addEventListener('click', function() {
+  const containerText = '(Standard Container: 40 Ft)';
+  const containerValue = containerText;
+  const currentText = document.getElementById('sub-text');
+  currentText.innerText = containerValue
+})
+document.getElementById('st-40HCft').addEventListener('click', function() {
+  const containerText = '(Standard Container: 40HC Ft)';
+  const containerValue = containerText;
+  const currentText = document.getElementById('sub-text');
+  currentText.innerText = containerValue
+})
+document.getElementById('st-45ft').addEventListener('click', function() {
+  const containerText = '(Standard Container: 45 Ft)';
+  const containerValue = containerText;
+  const currentText = document.getElementById('sub-text');
+  currentText.innerText = containerValue
+})
+
+
+// Refrigerated Container cross text 
+document.getElementById('rc-20ft').addEventListener('click', function() {
+  const containerText = '(Refrigerated Container: 20 Ft)';
+  const containerValue = containerText;
+  const currentText = document.getElementById('sub-text');
+  currentText.innerText = containerValue
+})
+document.getElementById('rc-40ft').addEventListener('click', function() {
+  const containerText = '(Refrigerated Container: 40 Ft)';
+  const containerValue = containerText;
+  const currentText = document.getElementById('sub-text');
+  currentText.innerText = containerValue
+})
+
+
+// Open top Container cross text 
+document.getElementById('otc-20ft').addEventListener('click', function() {
+  const containerText = '(Open Top Container: 20 Ft)';
+  const containerValue = containerText;
+  const currentText = document.getElementById('sub-text');
+  currentText.innerText = containerValue
+})
+document.getElementById('otc-40ft').addEventListener('click', function() {
+  const containerText = '(Open Top Container: 40 Ft)';
+  const containerValue = containerText;
+  const currentText = document.getElementById('sub-text');
+  currentText.innerText = containerValue
+})
+
+
+// Open top Container cross text 
+document.getElementById('frc-20ft').addEventListener('click', function() {
+  const containerText = '(Flat Rack Container: 20 Ft)';
+  const containerValue = containerText;
+  const currentText = document.getElementById('sub-text');
+  currentText.innerText = containerValue
+})
+document.getElementById('frc-40ft').addEventListener('click', function() {
+  const containerText = '(Flat Rack Container: 40 Ft)';
+  const containerValue = containerText;
+  const currentText = document.getElementById('sub-text');
+  currentText.innerText = containerValue
+})
+
+
+// Tank Container cross text 
+document.getElementById('tc-20ft').addEventListener('click', function() {
+  const containerText = '(Tank Container: 20 Ft)';
+  const containerValue = containerText;
+  const currentText = document.getElementById('sub-text');
+  currentText.innerText = containerValue
+})
+document.getElementById('tc-40ft').addEventListener('click', function() {
+  const containerText = '(Tank Container: 40 Ft)';
+  const containerValue = containerText;
+  const currentText = document.getElementById('sub-text');
+  currentText.innerText = containerValue
+})
+
+
